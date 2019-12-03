768597702a92cb52c843c4d2e15ca2eed2c7d6af
Your Name <you@example.com>
2019-12-03 23:06:29 +0400

    add start dynamic input

--- src/app/tasks/new/newtask.component.ts
+++ src/app/tasks/new/newtask.component.ts
@@ -1,17 +1,18 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { System, Env, DialogData } from 'src/app/_models';
-import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl, FormArray } from '@angular/forms';
+import { startWith, map } from 'rxjs/operators';
 
 @Component({
     selector: 'app-new-task-dialog',
     templateUrl: 'newtask.component.html',
     styleUrls: ['./newtask.component.css'],
 })
-export class NewTaskDialogComponent {
+export class NewTaskDialogComponent implements OnInit {
     systems: string[];
     meForm: FormGroup;
-
+    methodList: System[];
     constructor(
         public dialogRef: MatDialogRef<NewTaskDialogComponent>,
         @Inject(MAT_DIALOG_DATA) public data: DialogData, private fb: FormBuilder) {
@@ -22,6 +23,7 @@ export class NewTaskDialogComponent {
             environment: new FormControl(),
             system: new FormControl(),
             method: new FormControl(),
+            formData: new FormArray([]),
             // phone: new FormControl(),
             // email: new FormControl(),
             // femail: new FormControl(),
@@ -30,6 +32,24 @@ export class NewTaskDialogComponent {
             // password: new FormControl(),
             // newpassword: new FormControl(),
             // dblpassword: new FormControl(),
+        });
+    }
+    ngOnInit(): void {
+        this.meForm.get('system').valueChanges.subscribe((x: string) => {
+            this.methodList = this.data.systemsList[x];
+            console.log(this.methodList);
+        });
+        this.meForm.get('method').valueChanges.subscribe((x: System) => {
+            // console.log(x.methodParameters);
+            x.methodParameters.map(mparam => {
+                (this.meForm.get('formData') as FormArray).push(new FormGroup({
+                    type: new FormControl(mparam.name),
+                    value: new FormControl(mparam.required)
+                }));
+            });
+        });
+        this.meForm.get('formData').valueChanges.subscribe((x: string) => {
+            console.table(x);
         });
     }
 
